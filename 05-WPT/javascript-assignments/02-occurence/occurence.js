b2d79af3bfd35b9b9a8c4b79d5e1787b93896e6e
Abdul Ahad Sheikh <abdulahadsheikh@outlook.in>
2023-06-02 18:59:10 +0530

    Solution files deleted

--- 05-WPT/javascript-assignments/02-occurence/occurence.js
+++
@@ -1,8 +0,0 @@
-prnt_ocr = () => {
-    msg = document.getElementById("msg").value
-    ocr = parseInt(document.getElementById("ocr").value)
-
-    for (var i=0; i<ocr; i++){
-        document.getElementById("prnt_ocr").innerHTML+=msg + "<br>"
-    }
-}
--- 05-WPT/javascript-assignments/11-week-day/week-day.js
+++
@@ -1,9 +0,0 @@
-displayWeekday=()=>{
-    const weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-    date = new Date("2023-06-01")
-    day = weekday[date.getDay()]
-
-    return day
-}
-
-console.log(displayWeekday())
