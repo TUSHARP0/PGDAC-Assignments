44f1286d9252f0dae93dddb4147df2cef0199ca6
Abdul Ahad Sheikh <abdulahadsheikh@outlook.in>
2023-06-02 19:10:53 +0530

    solution uploaded

---
+++ 05-WPT/javascript-assignments/12-emp-array/emp-array.js
@@ -0,0 +1,13 @@
+emparr = [];
+
+for (var i = 0; i <= 5; i++) {
+  var name = prompt(`Enter the name of employee ${i + 1}:`)
+  emparr.push(name);
+}
+
+emparr.sort()
+
+console.log("Employee Names:");
+for (i = 0; i < emparr.length; i++) {
+  console.log(employees[i]);
+}
