a95ab5d9810899a9467a2bd696995c7a37f76f2f
Abdul Ahad Sheikh <abdulahadsheikh@outlook.in>
2023-06-02 19:10:57 +0530

    solution uploaded

---
+++ 05-WPT/javascript-assignments/13-emp-obj-array/emp-obj-array.js
@@ -0,0 +1,7 @@
+let employees = [
+    { id: 1, name: "John Doe", salary: 50000 },
+    { id: 2, name: "Jane Smith", salary: 60000 },
+    { id: 3, name: "Mike Johnson", salary: 55000 },
+    { id: 4, name: "Emily Davis", salary: 65000 }
+  ];
+  
