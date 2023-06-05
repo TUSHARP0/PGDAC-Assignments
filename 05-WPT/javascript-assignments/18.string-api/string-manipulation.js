247883fb06c85373924fa7464820883d674473be
Abdul Ahad Sheikh <abdulahadsheikh@outlook.in>
2023-06-05 17:30:24 +0530

    Button styling removed

--- 05-WPT/javascript-assignments/18.string-api/string-manipulation.js
+++ 05-WPT/javascript-assignments/18.string-api/string-manipulation.js
@@ -3,63 +3,54 @@ operate=()=>{
     sub_str = document.getElementById("sub_string").value
     rep_str = document.getElementById("rep_string").value
     var op = 0;
-
+    
     //To retrieve selected radio button
     var arr = document.getElementsByName("operations");
-        console.log(arr);
-        for (var i = 0; i < arr.length; i++) {
-          if (arr[i].checked) {
+    console.log(arr);
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].checked) {
             op = arr[i].value;
-          }}
+        }
+    }
     
-        switch(op){
+    switch(op){
         case "search":
-           
-                if (str.includes(sub_str)){
-                 document.getElementById("answer").innerHTML="<br>"+"Sub-string found"
-                }
-                else{
-                 document.getElementById("answer").innerHTML="<br>"+"Sub-string not found"
-                }
+            if (str.includes(sub_str)){
+                document.getElementById("answer").innerHTML="<br>"+"Sub-string found"
+            }
             
-             break;
-
-        case "upper_case":
-            
-                ans = str.toUpperCase()
-                document.getElementById("answer").innerHTML="<br>"+ ans
-            
-            
+            else{
+                document.getElementById("answer").innerHTML="<br>"+"Sub-string not found"
+            }
             break;
 
-        case "lower_case":
-            
-                ans = str.toLowerCase()
-                document.getElementById("answer").innerHTML="<br>"+ ans
-            
+            case "upper_case":
+            ans = str.toUpperCase()
+            document.getElementById("answer").innerHTML="<br>"+ ans
             break;
 
-        case "trim":
-            
-                ans = str.trim()
-                document.getElementById("answer").innerHTML="<br>"+ ans
-            
+            case "lower_case":
+            ans = str.toLowerCase()
+            document.getElementById("answer").innerHTML="<br>"+ ans
             break;
 
-        case "concat":
-            
-                ans = str.concat(sub_str)
-                document.getElementById("answer").innerHTML="<br>"+ ans
-            
+            case "trim":
+            ans = str.trim()
+            document.getElementById("answer").innerHTML="<br>"+ ans
             break;
 
-        case "replace":
-                
-                str = rep_str
-                document.getElementById("main_string").value = str
-                document.getElementById("answer").innerHTML="<br>"+ str
-            
+            case "concat":
+            ans = str.concat(sub_str)
+            document.getElementById("answer").innerHTML="<br>"+ ans
             break;
 
-    } //end of switch
-} //end of function
+            case "replace":
+            str = rep_str
+            document.getElementById("main_string").value = str
+            document.getElementById("answer").innerHTML="<br>"+ str
+            break;
+
+
+        } //end of switch
+
+    } //end of function
