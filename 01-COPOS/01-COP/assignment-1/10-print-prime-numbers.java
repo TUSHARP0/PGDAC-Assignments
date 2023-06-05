b85fd40fe246ec50bf45b0fe439d3c67a44fccf2
Abdul Ahad Sheikh <abdulahadsheikh@outlook.in>
2023-06-05 17:44:12 +0530

    Solution Uploaded

---
+++ 01-COPOS/01-COP/assignment-1/10-print-prime-numbers.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PrintPrimeNumbers {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter your number: ");
+        int num = in.nextInt();
+
+        System.out.println("Prime numbers between 0 and " + num + " are: ");
+
+        int counter = 0;
+        while(num!=1){
+            counter = 0;
+            for (int i=2 ; i<num ; i++){
+                if (num%i == 0){
+                    counter++;
+                    break;
+                }
+            }
+            if (counter==0) {
+                System.out.print(num + " ");
+            }
+            num--;
+        }
+
+    }
+}
