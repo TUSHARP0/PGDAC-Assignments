d9a3120ef7c9fbdea029313cff118b7627e3a9b3
Somesh Dattatraya Dahotre <131247687+totally-somesh@users.noreply.github.com>
2023-06-14 20:02:37 +0530

    Update largest_number.js
    ~ Here, in your JS program for finding largest number, If the user passes same numbers, then it may show incorrect output.
    ~ Hence, improved the conditional operator.

--- 05-WPT/javascript-assignments/01-largest-number/largest_number.js
+++ 05-WPT/javascript-assignments/01-largest-number/largest_number.js
@@ -3,15 +3,15 @@ num1 = parseInt(document.getElementById("num1").value)
 num2 = parseInt(document.getElementById("num2").value)
 num3 = parseInt(document.getElementById("num3").value)
 
-    if(num1>num2 && num1>num3){
+    if(num1>=num2 && num1>=num3){
         alert("Greatest number is : " + num1)
     }
 
-    if(num2>num3 && num2>num1){
+    if(num2>=num3 && num2>=num1){
         alert("Greatest number is : " + num1)
     }
 
-    if(num3>num2 && num3>num1){
+    if(num3>=num2 && num3>=num1){
         alert("Greatest number is : " + num1)
     }
 
