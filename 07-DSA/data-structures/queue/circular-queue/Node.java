fc801d2218d0e726b02605867a694b21fde2b4a6
Abdul Ahad Sheikh <95898825+gitabdulahad@users.noreply.github.com>
2023-07-03 21:00:46 +0530

    Create Node.java

---
+++ 07-DSA/data-structures/queue/circular-queue/Node.java
@@ -0,0 +1,23 @@
+//Creating a Node class
+public class Node<T> {
+    // Initialization of variables
+    private T element;
+    private Node<T> next;
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    public T getElement() {
+        return element;
+    }
+
+    public Node(T element) {
+        this.element = element;
+        this.next = null;
+    }
+}
